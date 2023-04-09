609f638faed7518a8c3ed41f793689dd857ba39a
Shnob <jake.dombnz@gmail.com>
2023-04-09 18:04:54 +1200

    Started working on Cube and Piece structs, not much in there yet

--- src/rubiks_cube.rs
+++ src/rubiks_cube.rs
@@ -1,14 +1,30 @@
 use colored::Colorize;
 
 pub struct Cube {
+    pieces: Vec<Piece>,
 }
 
 struct Piece {
+    verts: Vec<(f32, f32, f32)>,
+    rgb: (u8, u8, u8),
+}
 
+impl Cube {
+    fn show(&self) {
+        for piece in self.pieces.iter() {
+        }
+    }
 }
 
 impl Default for Cube {
     fn default() -> Cube {
-        Cube {}
+        Cube {
+            pieces: vec![
+                Piece {
+                    verts: vec![(-10.0, -10.0, 0.0), (-10.0, 10.0, 0.0), (10.0, 10.0, 0.0), (10.0, -10.0, 0.0)],
+                    rgb: (255, 0, 255),
+                }
+            ],
+        }
     }
 }
